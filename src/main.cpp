3f34cee858c9b10761a31862ec5d068f05477b12
Arazorg <40016355+Arazorg@users.noreply.github.com>
2018-12-18 18:34:39 +0700

    main.cpp
    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    for(int i = 0; i < argc; i ++)
+    {
+        qDebug()<< i << " " << argv[i];
+    }
     qDebug()<<"HI";
     exit(0);
     return a.exec();
