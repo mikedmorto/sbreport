632331491ae8ae89d343b2e9329a5923a93ce360
Arazorg <40016355+Arazorg@users.noreply.github.com>
2019-02-15 19:13:23 +0700

    evenVec

--- src/sbdata.cpp
+++ src/sbdata.cpp
@@ -64,29 +64,39 @@ bool SBdata::loadEvent(const QString &path)
      QTextStream in(&eventFile);
      in.setCodec("UTF-8");
      qDebug()<<"start cycle of loading";
+     // todo : delete this out for debug
      QTextStream out(stdout);
      out.setCodec("UTF-8");
+     ///////
+
+
      while(!in.atEnd()){
          QString line = in.readLine();
 //         qDebug() << "The current string " << line;
          QStringList sl = line.split('\t');
 
 //         qDebug() << sl;
-        if(sl.size() < 6)
+        if(sl.size() < 6 and sl.size() > 10)
             continue;
 
         //qDebug() << sl.at(5);
         out<< sl.at(5) << endl;
 
-//        if(sl.at(5).contains(QRegExp("^(([0-9]*|парковка):Вход.*|([0-9]*|парковка):Выход.*)$")))
-        if(sl.at(5).contains(QRegExp(QString::fromUtf8("^.*\:Вход.*$")))){
-            qDebug() << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
-        }else{
-            qDebug() << "FALSE";
+        if(sl.at(5).contains(QRegExp("^(([0-9]*|парковка):Вход.*|([0-9]*|парковка):Выход.*)$"))){
+//        if(sl.at(5).contains(QRegExp(QString::fromUtf8("^.*\:Вход.*$")))){
+//            qDebug() << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+            EventInfo ei;
+//            qDebug()<<sl.at(0);
+            ei.date = QDate::fromString(sl.at(0),"dd.MM.yyyy");
+            ei.time = QTime::fromString(sl.at(1), "hh:mm:ss");
+            ei.event = sl.at(5);
+            ei.person = sl.at(8);
+            this->evenVec.append(ei);
         }
 
      }
-     qDebug()<<"end load event file";
+     qDebug()<< "evenVec size == " << evenVec.size();
+     qDebug() <<"end load event file";
 
 }
 void SBdata::clearEvent()
