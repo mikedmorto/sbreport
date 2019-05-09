5d20237194cd344ab0a7c6d4ae46a6f6cd196d98
Arazorg <arazorggg@gmail.com>
2019-05-09 18:57:15 +0700

    clear code
    delete indent, space, unnecessary strings
    adding \n

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "sbconfig.h"
 #include "sbdata.h"
 
-
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -14,7 +13,6 @@ int main(int argc, char *argv[])
     a.setApplicationName(APP_NAME);
     a.setApplicationVersion(GIT_VERSION);
     a.setOrganizationName(CORP_NAME);
-
     QCommandLineParser parser;
     parser.addHelpOption();
     parser.addOption({{"v","version"}, QCoreApplication::tr("Show version")});
@@ -34,7 +32,6 @@ int main(int argc, char *argv[])
     QCommandLineOption target_month(QStringList() << "m" << "target-month",
                                       QCoreApplication::translate("main", "Target month format MM.yyyy like 12.2012"),
                                       QCoreApplication::translate("main", "month"));
-
     parser.addOption(fio_fileOption);
     parser.addOption(target_fileOption);
     parser.addOption(output_fileOption);
@@ -43,92 +40,69 @@ int main(int argc, char *argv[])
 
     SBconfig cfg;
     SB::SBdata data;
-    if(parser.isSet("f"))
-    {
+    if(parser.isSet("f")){
         cfg.setFioPath(parser.value("f"));
          QTextStream(stdout) << data.getCurrentTime() << "fio-file = " << cfg.getFioPath() << endl;
-        //qDebug()<<"fio-file = " << cfg.getFioPath();
+//        qDebug()<<"fio-file = " << cfg.getFioPath();
     }
-    if(parser.isSet("t"))
-    {
+    if(parser.isSet("t")){
         cfg.setTargetPath(parser.value("t"));
          QTextStream(stdout)<< data.getCurrentTime() << "target-file = " << cfg.getTargetPath() << endl;
-        //qDebug()<<"target-file = " << cfg.getTargetPath();
+//        qDebug()<<"target-file = " << cfg.getTargetPath();
     }
-    if(parser.isSet("o"))
-    {
+    if(parser.isSet("o")){
         cfg.setOutputPath(parser.value("o"));
          QTextStream(stdout)<< data.getCurrentTime() << "output-file = " << cfg.getOutputPath() << endl;
-        //qDebug()<<"output-file = " << cfg.getOutputPath();
+//       qDebug()<<"output-file = " << cfg.getOutputPath();
     }
-    if(parser.isSet("m"))
-    {
+    if(parser.isSet("m")){
         cfg.setTargetDate(parser.value("m"));
-
         if(!cfg.isValidDate()){
-            QTextStream(stdout)<< data.getCurrentTime() << parser.value("m") <<" - invalid date time format " << endl;
             QTextStream(stderr)<< data.getCurrentTime() << parser.value("m") <<" - invalid date time format " << endl;
             exit(1);
         }
     }
-    if(parser.isSet("version"))
-    {
+    if(parser.isSet("version")){
         QTextStream(stdout)<< data.getCurrentTime() << "Current version " << a.applicationVersion() << endl;
-        //qDebug()<<a.applicationVersion();
+//        qDebug()<<a.applicationVersion();
         exit(0);
     }
-
-
-    // compare keys
+//     compare keys
     bool isOK = true;
     if(parser.isSet("f")){
         if(!parser.isSet("t"))
                 isOK = false;
-        // others
+//         others
         if(!parser.isSet("m"))
             isOK = false;
     }
-
     if(!isOK){
-        // fail params
-        QTextStream(stdout)<< data.getCurrentTime() << "Failure params" << endl;
+//         fail params
         QTextStream(stderr)<< data.getCurrentTime() << "Failure params" << endl;
-        //qDebug()<<"Failure params";
+//        qDebug()<<"Failure params";
         exit(1);
     }
-
-
-
-
     if(!data.loadFio(cfg.getFioPath())){
-        QTextStream(stdout)<< data.getCurrentTime() << "Main error = " << data.getLastError();
         QTextStream(stderr)<< data.getCurrentTime() << "Main error = " << data.getLastError();
-        //qDebug() << "Main error = " << data.getLastError();
+//        qDebug() << "Main error = " << data.getLastError();
         exit(1);
     }
 
     if(!data.loadEvent(cfg.getTargetPath(), cfg.getTargetDate()))
     {
-        QTextStream(stdout)<< data.getCurrentTime() << "Main error = " << data.getLastError();
         QTextStream(stderr)<< data.getCurrentTime() << "Main error = " << data.getLastError();
-        //qDebug() << "Main error = " << data.getLastError();
+//        qDebug() << "Main error = " << data.getLastError();
         exit(1);
     }
 
-    // processing
-
+//     processing
     data.processing();
-
     if(!data.saveOutput(cfg.getOutputPath()))
     {
-        QTextStream(stdout)<< data.getCurrentTime() << "Main error = " << data.getLastError();
         QTextStream(stderr)<< data.getCurrentTime() << "Main error = " << data.getLastError();
-        //qDebug() << "Main error = " << data.getLastError();
+//        qDebug() << "Main error = " << data.getLastError();
         exit(1);
     }
-
-
-
     exit(0);
     return a.exec();
 }
--- src/sbdata.cpp
+++ src/sbdata.cpp
@@ -10,36 +10,28 @@ SBdata::SBdata()
 const QString SBdata::getLastError(){
    return this->lastError;
 }
-
 bool SBdata::loadFio(const QString &path)
 {
     this->fioList.clear();
-    //qDebug() << "opening file " << path;
+//    qDebug() << "opening file " << path;
     QFile fioFile;
-    fioFile.setFileName(path);
+    fioFile.setFileName(path);   
     if(!fioFile.open(QIODevice::ReadOnly | QIODevice::Text)){
-        this->lastError = "cannot open file " + path + " because of ";
-        this->lastError += fioFile.errorString();
-         //qDebug() << this->lastError;
-
+        this->lastError = "cannot open file " + path + ",because of ";
+        this->lastError += fioFile.errorString() + "\n";
+//         qDebug() << this->lastError;
          return false;
     }
-
-
     QTextStream in(&fioFile);
     in.setCodec("UTF-8");
-
-
     while(!in.atEnd()){
         QString line = in.readLine();
-        //qDebug() << "The current string " << line << endl;
+//      qDebug() << "The current string " << line << endl;
         this->fioList.append(line);
     }
-
-    //qDebug() << fioList;
-    //qDebug() << "closing the file" + path;
+//    qDebug() << fioList;
+//    qDebug() << "closing the file" + path;
     fioFile.close();
-
     return true;
 }
 
@@ -50,66 +42,57 @@ void SBdata::clearFio()
 
 bool SBdata::loadEvent(const QString &path, const QDate &targetDate)
 {
-    //qDebug()<<"Load event start";
+//  qDebug()<<"Load event start";
     this->clearEvent();
     QFile eventFile;
-
     this->targetDate = targetDate;
-
     eventFile.setFileName(path);
      if(!eventFile.open(QIODevice::ReadOnly | QIODevice::Text)){
-         this->lastError = "cannot open file " + path + " because of ";
-         this->lastError += eventFile.errorString();
-          //qDebug() << this->lastError;
-
-          return false;
+         this->lastError = "cannot open file " + path + "because of " + "\n";
+         this->lastError += eventFile.errorString() + "\n";
+//       qDebug() << this->lastError;
+         return false;
      }
      QTextStream in(&eventFile);
      in.setCodec("UTF-8");
-     //qDebug()<<"start cycle of loading";
-     // todo : delete this out for debug
+//     qDebug()<<"start cycle of loading";
+//     todo : delete this out for debug
      QTextStream out(stdout);
      out.setCodec("UTF-8");
 
-
-
      while(!in.atEnd()){
          QString line = in.readLine();
-//         qDebug() << "The current string " << line;
+//       qDebug() << "The current string " << line;
          QStringList sl = line.split('\t');
-
-//         qDebug() << sl;
+//      qDebug() << sl;
         if(sl.size() < 6 and sl.size() > 10)
             continue;
-
-        //qDebug() << sl.at(5);
-        // out<< sl.at(0) << endl;
+//      qDebug() << sl.at(5);
+//      out<< sl.at(0) << endl;
         QDate tdate = QDate::fromString(sl.at(0),"dd.MM.yyyy");
-
         if( !((tdate.year() == targetDate.year()) && (tdate.month() == targetDate.month())) )
              continue;
-
         if(sl.at(5).contains(QRegExp("^(([0-9]*|парковка):Вход.*|([0-9]*|парковка):Выход.*)$"))){
-//        if(sl.at(5).contains(QRegExp(QString::fromUtf8("^.*\:Вход.*$")))){
-//            qDebug() << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+//          if(sl.at(5).contains(QRegExp(QString::fromUtf8("^.*\:Вход.*$")))){
+//          qDebug() << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
             EventInfo ei;
-//            qDebug()<<sl.at(0);
+//          qDebug()<<sl.at(0);
             ei.date = QDate::fromString(sl.at(0),"dd.MM.yyyy");
             ei.time = QTime::fromString(sl.at(1), "h:mm:ss");
             ei.event = sl.at(5);
             ei.person = sl.at(8);
 
-            // testing of date time
+//          testing of date time
             if(ei.date.isValid() and ei.time.isValid()){
                 this->evenVec.append(ei);
             }
         }
-
      }
-     //qDebug()<< "evenVec size == " << evenVec.size();
-     //qDebug() <<"end load event file";
+//     qDebug()<< "evenVec size == " << evenVec.size();
+//     qDebug() <<"end load event file";
     return true;
 }
+
 void SBdata::clearEvent()
 {
     this->evenVec.clear();
@@ -119,74 +102,49 @@ void SBdata::processing()
 {
     QRegExp regEnter("^([0-9]*|парковка):Вход.*$");
     QRegExp regExit ("^([0-9]*|парковка):Выход.*$");
-
-
-    //qDebug() << "start";
+//  qDebug() << "start";
     int lastDay = this->targetDate.daysInMonth();
     this->vOut.clear();
-    //qDebug() << "last day: " << lastDay;
+//  qDebug() << "last day: " << lastDay;
     for(int i = 0; i < fioList.size(); i++)
     {
-        //qDebug() << "person " << fioList.at(i);
+//      qDebug() << "person " << fioList.at(i);
         OutputData outD;
-
         outD.person = fioList.at(i);
-
         for( int k = 1 ; k <= lastDay; k++){
-
             QVector<EventInfo> personEV;
             for(int j = 0; j < evenVec.size(); j++)
             {
-                // qDebug()<<"A - "<< evenVec.at(j).person;
-                // qDebug()<<"B - "<< fioList.at(i);
-
+//                 qDebug()<<"A - "<< evenVec.at(j).person;
+//                 qDebug()<<"B - "<< fioList.at(i);
                 if(evenVec.at(j).person == fioList.at(i)
                         and evenVec.at(j).date.day() == k ){
-                // qDebug()<<"OKKKK";
-
-                   // if(!(evenVec.at(j).event.isEmpty())) {
-                         personEV.append(evenVec.at(j));
-                   // }
-
+                         personEV.append(evenVec.at(j));    
                 }
-
             }
-            //if(personEV.size() != 0) {
-               // qDebug() << QString("Day %1 event size %2").arg(k).arg(personEV.size());
-            //}
             EventInfo firstEnter;
-            EventInfo lastExit;
-
-
-            //два множества событий всех входов и всех выходов
-
-
+            EventInfo lastExit;          
+//          два множества событий всех входов и всех выходов
             for(int l = 0; l < personEV.size(); l ++) {
 //                qDebug()<<"EVENT" << personEV.at(l).event << " - "<< personEV.at(l).date << "="<< personEV.at(l).time;
-
-
                 if(personEV.at(l).event.contains(regEnter)) {
                     if(firstEnter.isNull){
                         firstEnter = personEV.at(l);
                         firstEnter.isNull = false;
                         continue;
                     }
-
                     if(firstEnter.time > personEV.at(l).time){
                         firstEnter = personEV.at(l);
                         continue;
                     }
                     continue;
                 }
-
-
                 if(personEV.at(l).event.contains(regExit)){
                     if(lastExit.isNull){
                         lastExit = personEV.at(l);
                         lastExit.isNull = false;
                         continue;
                     }
-
                     if(lastExit.time < personEV.at(l).time){
                         lastExit = personEV.at(l);
                         continue;
@@ -196,16 +154,12 @@ void SBdata::processing()
             }
 //            qDebug() << "firstEnter " << firstEnter.time.toString(FORMAT_TIME);
 //            qDebug() << "lastExit" << lastExit.time.toString(FORMAT_TIME);
-
              outD.vFirstEnter.append(firstEnter.time);
              outD.vLastExit.append(lastExit.time);
-
-             // analytic first input, last output
+//             analytic first input, last output
         }
         vOut.append(outD);
     }
-
-
     //qDebug() << "end";
     //qDebug() << vOut.size();
 }
@@ -218,29 +172,24 @@ bool SBdata::saveOutput(const QString savePath)
     fo.setFileName(fname);
     if(!fo.open(QIODevice::ReadWrite | QIODevice::Truncate | QIODevice::Text)){
        this->lastError = "cannot open file " + fname + " because of ";
-       this->lastError += fo.errorString();
+       this->lastError += fo.errorString() + "\n";
        return false;
     }
-
     for(int i = 0; i < vOut.size(); i++){
         QString ret;
-
         ret.append(vOut.at(i).person);
         for(int j = 0; j < vOut.at(i).vFirstEnter.size(); j++){
             ret.append(",");
              ret.append(vOut.at(i).vFirstEnter.at(j).toString("hh:mm:ss"));
              ret.append("-");
              ret.append(vOut.at(i).vLastExit.at(j).toString("hh:mm:ss"));
-
         }
-    ret.append("\n");
-    fo.write(ret.toUtf8());
+        ret.append("\n");
+        fo.write(ret.toUtf8());
     }
     fo.close();
     return true;
-
 }
-
 QString SBdata::getCurrentTime()
 {
     QString strCurretTime = QDateTime::currentDateTime().toString("yyyy-MM-ddThh:mm:ss ");
